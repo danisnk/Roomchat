932f536a4cacabb11e418f519006992e88a908fb
danisnk <84008707+danisnk@users.noreply.github.com>
2021-08-02 16:05:36 +0530

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -1,12 +1,7 @@
 import firebase from 'firebase';
 
 const firebaseApp = firebase.initializeApp({
-    apiKey: "AIzaSyBzd59x-1FWay9DWLT9hFRkVedvplSkK2A",
-    authDomain: "pub-chat-6b067.firebaseapp.com",
-    projectId: "pub-chat-6b067",
-    storageBucket: "pub-chat-6b067.appspot.com",
-    messagingSenderId: "769537015294",
-    appId: "1:769537015294:web:544ded4ef3a39cd094d4af"
+    //your api
 })
 
 const db = firebaseApp.firestore()
